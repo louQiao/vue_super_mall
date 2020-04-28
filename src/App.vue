49bf08c805ab811cfc75e63ae7d8b396aac748d2
louqiao <13831160723@139.com>
2020-04-28 20:47:27 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    a vue project
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
